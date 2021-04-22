22c71a9c891ef28bafb677d700ebe6723de618cb
Amanda Cleto <amanda.clet@gmail.com>
2021-04-21 22:19:19 -0300

    creating routes and installing sass
    Co-authored-by: caioliveira277 <caioliveira277@users.noreply.github.com>

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,16 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
   <router-view/>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  @import url('https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&display=swap');
+  * {
+    box-sizing: border-box;    
+    padding: 0;
+    margin: 0;
+    outline: none;
+    font-family: 'DM Serif Display', serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
-}
 </style>

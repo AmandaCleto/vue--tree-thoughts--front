94cd40d2554eb667441e858e4d62f349fcaaf7a6
Amanda Cleto <amanda.clet@gmail.com>
2021-05-24 23:51:26 -0300

    creating mask for dropdown

---
+++ src/components/Mask.vue
@@ -0,0 +1,24 @@
+<template>
+	<teleport to="body">
+		<div class="mask" @click="$emit('handlerMask', {state: true})"></div>
+	</teleport>
+</template>
+<script>
+export default {
+	name: 'Mask',
+	emits: ['handlerMask']
+}
+</script>
+<style lang="scss" scoped>
+	.mask {
+		// background: transparent;
+		background: purple;
+		position: fixed;
+		width: 10vw;
+		height: 100vh;
+		top: 0;
+		right: 0;
+		z-index: 10;
+		// pointer-events: none;
+	}
+</style>

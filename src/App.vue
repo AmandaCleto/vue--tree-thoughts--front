5fc1a04a787ab5d7b462dbab8d83bc1ae7c433e5
Amanda Cleto <amanda.clet@gmail.com>
2021-05-24 23:52:24 -0300

    starting the creation of  flip for the new card component

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,47 @@
 <template>
-  <router-view/>
+    <router-view />
 </template>
 
 <style lang="scss">
-  @import url('https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&display=swap');
-  @import url('https://fonts.googleapis.com/css2?family=Didact+Gothic&display=swap');
-  * {
-    box-sizing: border-box;    
+@import url("https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Didact+Gothic&display=swap");
+* {
+    box-sizing: border-box;
     padding: 0;
     margin: 0;
     outline: none;
-    font-family: 'DM Serif Display', serif;
+    font-family: "DM Serif Display", serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-  }
-  .container {
+}
+.container {
     width: 100%;
     max-width: 1140px;
     span.separator {
-      width: 500px;
-      height: 180px;
-      align-self: center;
+        width: 500px;
+        height: 180px;
+        align-self: center;
     }
 
-    & > .separator:nth-of-type(even){
-      background: url('~@/assets/line-dashed-right.svg') no-repeat;
-    }
-    & > .separator:nth-of-type(odd){
-      background: url('~@/assets/line-dashed-left.svg') no-repeat;
-    }
-
-    & > .card:nth-of-type(even) {
-      align-self: flex-end;
-    }
-    & > .card:nth-of-type(odd) {
-      align-self: flex-start;
-    }
-
-  }
+    // & > .group {
+    //     display: contents;
+		
+	& > :nth-of-type(even) {
+		.separator {
+			background: url("~@/assets/line-dashed-left.svg") no-repeat;
+		}
+		.card {
+			align-self: flex-end;
+		}
+	}
+	& > :nth-of-type(odd) {
+		.separator {
+			background: url("~@/assets/line-dashed-right.svg") no-repeat;
+		}
+		.card {
+			align-self: flex-start;
+		}
+	}
+    // }
+}
 </style>
